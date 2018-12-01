fb94facab44c890dbd1e7122966b8a03d98e4a79
Ellibereth <eli.chang.iv@gmail.com>
2018-12-01 05:46:00 -0500

    Update plugin.rb
    Registered fa 5 icons

--- plugin.rb
+++ plugin.rb
@@ -4,3 +4,13 @@
 # authors: Régis Hanol, Sam Saffron
 
 register_asset "stylesheets/bbcode.scss"
+
+register_svg_icon "fa-underline" if respond_to?(:register_svg_icon)
+register_svg_icon "fa-indent" if respond_to?(:register_svg_icon)
+register_svg_icon "fa-align-left" if respond_to?(:register_svg_icon)
+register_svg_icon "fa-align-center" if respond_to?(:register_svg_icon)
+register_svg_icon "fa-align-right" if respond_to?(:register_svg_icon)
+register_svg_icon "fa-align-justify" if respond_to?(:register_svg_icon)
+register_svg_icon "fa-palette" if respond_to?(:register_svg_icon)
+register_svg_icon "fa-font" if respond_to?(:register_svg_icon)
+register_svg_icon "fa-gavel" if respond to?(:register_svg_icon)
